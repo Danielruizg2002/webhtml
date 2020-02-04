007fc2ecdf35574bbb8056a43bcb00252a5e69f0
Daniel Ruiz Guinea <60671679+Danielruizg2002@users.noreply.github.com>
2020-02-04 21:37:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function(){
+	$('.up').hide();
+	$('.up').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+ 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 200 ){
+			$('.up').slideDown(300);
+		} else {
+			$('.up').slideUp(300);
+		}
+	});
+ 
+});
+
+
